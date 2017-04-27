532d930c63e5ea1404e6a5f19eb6f42e709fe1d1
Lukas Eklund <leklund@fastly.com>
2017-04-27 13:10:50 -0500

    control that cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Clearance::Controller
+
+  before_action :cache_control
+
+  def cache_control
+    expires_in 1.day, public: true, must_revalidate: true
+    request.session_options[:skip] = true
+  end
 end
